53f7759fa95e090f8fe1f42f3fa536f7af0bbcb0
m-kid <maciej.kidawa2@gmail.com>
2016-10-08 20:03:21 +0200

    dodałem nazwę pliku ze zdjęciami do gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
         files: [{
             expand: true,
             cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
+            src: ['images/*.{png,jpg,gif}'],
             dest: 'images/build/'
         }]
     }
